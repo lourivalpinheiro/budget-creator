be7b991d309859593f983a8fa5718868e78ef29c
Lourival Pinheiro <lourivaltpneto@outlook.com>
2025-04-19 14:10:13 -0300

    Text Correction

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def budget_calculate():
         st.info(body= " A divisão ideal do seu salário deveria ser: \n 1. {:.2f} para impostos; \n 2. {:.2f} para reserva de emergência; \n 3. {:.2f} para investimentos; \n 4. {:.2f} para despesas gerais.".format((calculus * 10)/100, (calculus * 15.00)/100 , (calculus * 15.00)/100, (calculus * 60)/100), icon="ℹ️")  
         st.write("Lembre-se que as porcentagens podem variar de acordo com a sua realidade, entretanto, caso suas despesas gerais ou impostos fiquem abaixo do valor estipulado, é interessante que você destine esse valor à sua reserva de emergência e investimentos.")
 
-salary = st.number_input(label="INSIRA O SEU SALÀRIO", key="salary")
+salary = st.number_input(label="INSIRA O SEU SALÁRIO", key="salary")
 submit = st.button(label="CALCULAR", key="calculate", on_click=budget_calculate)
